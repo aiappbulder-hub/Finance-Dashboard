a9a6a9483bd6d3fba476a2cd9f433a6c63d50be2
aiappbulder-hub <trentgauci@gmail.com>
2026-05-19 09:18:26 +1000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'hvac-tool-v1';
+const ASSETS = [
+  '/hvac-tool/',
+  '/hvac-tool/index.html',
+  '/hvac-tool/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
